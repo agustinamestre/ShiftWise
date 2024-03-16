d4ec09a47f85209ecab1269de7c68f9035e09eb1
Agustina Mestre <you@example.com>
2024-03-16 00:03:47 -0300

    Agregando proyecto de Spring Boot

---
+++ ShiftWise-Backend/src/main/java/com/agustinamestre/ShiftWiseBackend/ShiftWiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agustinamestre.ShiftWiseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftWiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftWiseBackendApplication.class, args);
+	}
+
+}
---
+++ ShiftWise-Backend/src/test/java/com/agustinamestre/ShiftWiseBackend/ShiftWiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustinamestre.ShiftWiseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftWiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
