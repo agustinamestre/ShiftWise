7f67d74e8810847e23c3e975d80197d8e5002d1e
amestre <agustinamestre@macro.com.ar>
2024-08-01 23:30:35 -0300

    validacion fecha de nacimiento

---
+++ ShiftWise-Backend/src/main/java/com/agustinamestre/ShiftWiseBackend/shared/validators/adulto/Adulto.java
@@ -0,0 +1,17 @@
+package com.agustinamestre.ShiftWiseBackend.shared.validators.adulto;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = AdultoValidator.class)
+public @interface Adulto {
+    String message() default "La edad del empleado no puede ser menor a 18 años.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
