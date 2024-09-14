3fe01b67f7c864cfff104d266f4abe18fbe44f23
amestre <agustinamestre@macro.com.ar>
2024-09-14 16:35:25 -0300

    Ok log

--- ShiftWise-Backend/src/main/java/com/agustinamestre/ShiftWiseBackend/ShiftWiseBackendApplication.java
+++ ShiftWise-Backend/src/main/java/com/agustinamestre/ShiftWiseBackend/ShiftWiseBackendApplication.java
@@ -1,13 +1,26 @@
 package com.agustinamestre.ShiftWiseBackend;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShiftWiseBackendApplication {
+	static Logger logger = LogManager.getLogger(ShiftWiseBackendApplication.class);
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ShiftWiseBackendApplication.class, args);
+
+		logger.info("  ____   _  __");
+		logger.info(" / __ \\ | |/ /");
+		logger.info("| |  | || ' / ");
+		logger.info("| |  | ||  <  ");
+		logger.info("| |__| || . \\ ");
+		logger.info(" \\____(_)_|\\_\\");
+
+
 	}
 
 }
