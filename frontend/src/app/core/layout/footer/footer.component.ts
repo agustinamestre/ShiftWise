7ea4b9c098c936583964ac8dd71d4a9253105fc8
amestre <agustinamestre@macro.com.ar>
2025-03-19 01:09:35 -0300

    Initial design: Navbar, footer, and login form

---
+++ frontend/src/app/core/layout/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MaterialModule } from '../../../material/material.module';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {}
---
+++ frontend/src/app/core/layout/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MaterialModule } from '../../../material/material.module';
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {}
