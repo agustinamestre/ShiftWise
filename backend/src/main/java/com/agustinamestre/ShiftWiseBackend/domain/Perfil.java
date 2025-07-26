6ba7e5fd0e79f03d17188e6d3567618e5dc4e086
amestre <agustinamestre@macro.com.ar>
2025-07-26 15:38:12 -0300

    conceptos, perfil de usuario y permisos de usuario

---
+++ backend/src/main/java/com/agustinamestre/ShiftWiseBackend/domain/Perfil.java
@@ -0,0 +1,38 @@
+package com.agustinamestre.ShiftWiseBackend.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.Set;
+
+/**
+ * Entidad que representa los perfiles de usuarios en el sistema.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "perfiles")
+public class Perfil {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPerfil;
+    private String nombre;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            joinColumns = @JoinColumn(name = "id_perfil"),
+            inverseJoinColumns = @JoinColumn(name = "id_permiso")
+    )
+    private Set<Permiso> permisos;
+
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
