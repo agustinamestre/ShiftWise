0d623bf6aa1adfe31d2f6e8a48444f5d58ccccbf
amestre <agustinamestre@macro.com.ar>
2025-08-13 23:03:43 -0300

    fix error message

--- frontend/src/app/user/services/User.service.ts
+++ frontend/src/app/user/services/User.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import UserRequest from "../interfaces/UserRequest";
-import { Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 import { UserResponse } from "../interfaces/UserResponse";
 import { environment } from "../../../environments/environment";
+import ErrorResponse from "../../models/ErrorResponse";
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +16,9 @@ export class UserService {
   createUser(request: UserRequest): Observable<UserResponse> {
     let url = environment.api + this.endpoint;
 
-    return this.http.post<UserResponse>(url, request);
+    return this.http
+      .post<UserResponse>(url, request)
+      .pipe(catchError(this.handleError));
   }
 
   getUserById(id: string): Observable<UserResponse> {
@@ -26,5 +29,37 @@ export class UserService {
   getUsers(): Observable<UserResponse[]> {
     let url = environment.api + this.endpoint;
     return this.http.get<UserResponse[]>(url);
+  }
+
+  updateUser(id: string, request: UserRequest): Observable<UserResponse> {
+    let url = environment.api + this.endpoint + '/' + id;
+
+    return this.http
+      .put<UserResponse>(url, request)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let backendErrors: any[] = [];
+
+    if (error.error && Array.isArray(error.error.error)) {
+      backendErrors = error.error.error;
+    }
+
+    const errorResponse: ErrorResponse = {
+      genericErrorMessage:
+        backendErrors.length > 0
+          ? backendErrors[0].errorMessage
+          : 'Ocurrió un error inesperado',
+      statusCode: error.status,
+      propertyErrors: backendErrors.map((e: any) => ({
+        property: e.errorCode
+          ?.replace('_invalido', '')
+          .replace('_requerido', ''),
+        error: e.errorMessage,
+      })),
+    };
+
+    return throwError(() => errorResponse);
   }
 }
