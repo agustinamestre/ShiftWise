d4840978ba3958e64af4f22b1d697ffeb370b09f
amestre <agustinamestre@macro.com.ar>
2025-09-06 15:22:30 -0300

    buscar jornadas

--- frontend/src/app/dashboard/jornadas/services/jornada.service.ts
+++ frontend/src/app/dashboard/jornadas/services/jornada.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { JornadaRequest } from "../interface/JornadaRequest";
 import { environment } from "../../../../environments/environment";
 import Jornada from "../interface/Jornada";
+import { JornadaResponse } from '../interface/JornadaResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -16,5 +17,22 @@ export class JornadaService {
   createJornada(jornada: JornadaRequest): Observable<Jornada> {
     const url = `${environment.api}${this.endpoint}`;
     return this.http.post<Jornada>(url, jornada);
+  }
+
+  getJornadas(nroDocumento?: string, fecha?: string, apellido?: string): Observable<JornadaResponse[]> {
+    const url = `${environment.api}${this.endpoint}`;
+
+    let params = new HttpParams();
+
+    if (nroDocumento) {
+      params = params.set('nroDocumento', nroDocumento);
+    }
+    if (apellido) {
+      params = params.set('apellido', apellido);
+    }
+    if (fecha) {
+      params = params.set('fecha', fecha);
+    }
+    return this.http.get<JornadaResponse[]>(url, { params });
   }
 }
